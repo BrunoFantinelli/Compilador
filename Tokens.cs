14f4c37631375673db8e5c39ea2f1c24544c0655
BrunoFantinelli <bruno.fantinelli0@gmail.com>
2017-05-03 22:42:08 -0300

    Add files via upload

---
+++ Tokens.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Tokens
+    {
+
+        public static int Covert(string type)
+        {
+            if (type.Equals("ERR"))
+            {
+                return -2;
+            }
+            else if (type.Equals("EOL"))
+            {
+                return 256;
+            }
+            else if (type.Equals("PLUS"))
+            {
+                return 257;
+            }
+            else if (type.Equals("EQUALS"))
+            {
+                return 258;
+            }
+            else if (type.Equals("NUM"))
+            {
+                return 259;
+            }
+            else if (type.Equals("VAR"))
+            {
+                return 260;
+            }
+            else if (type.Equals("PRINT"))
+            {
+                return 261;
+            }
+
+            return -1;
+        }
+    }
+
+}
+}
